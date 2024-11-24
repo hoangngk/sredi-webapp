ea16457746be524b6052d0e418b7c68fee83aa8e
Hoang NK <n.khanhhoang@gmail.com>
2024-11-24 11:33:50 +0700

    feat: install angular material, create sample custom theme

--- src/app/core/layouts/sidenav-content.layout.ts
+++ src/app/core/layouts/sidenav-content.layout.ts
@@ -3,17 +3,31 @@ import { SidenavComponent } from './sidenav/sidenav.component';
 import { RouterModule } from '@angular/router';
 import { HeaderComponent } from './header/header.component';
 import { BreadcrumbComponent } from './breadcrumb/breadcrumb.component';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-sidenav-content-layout',
   standalone: true,
   imports: [
+    CommonModule,
     SidenavComponent,
     HeaderComponent,
     BreadcrumbComponent,
     RouterModule,
+    MatSidenavModule
   ],
-  template: ` <div><router-outlet></router-outlet></div> `,
+  template: `
+   <mat-sidenav-container class="side-nav-layout-container">
+    <mat-sidenav mode="side" opened>
+      <app-sidenav></app-sidenav>
+    </mat-sidenav>
+    <mat-sidenav-content>
+      <app-header></app-header>
+      <app-breadcrumb></app-breadcrumb>
+      <router-outlet></router-outlet>
+    </mat-sidenav-content>
+  </mat-sidenav-container>`,
   styles: [],
   animations: [],
 })
--- src/app/core/layouts/sidenav/sidenav.component.ts
+++ src/app/core/layouts/sidenav/sidenav.component.ts
@@ -1,8 +1,11 @@
+import { CommonModule } from '@angular/common';
 import { Component, ViewEncapsulation } from '@angular/core';
+import { MatSidenavModule } from '@angular/material/sidenav';
 
 @Component({
   selector: 'app-sidenav',
   standalone: true,
+  imports: [CommonModule, MatSidenavModule],
   templateUrl: './sidenav.component.html',
   styleUrl: './sidenav.component.scss',
   encapsulation: ViewEncapsulation.None,
